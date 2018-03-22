d77cf4a49ef4b109f8e37731e64080b78e70746c
Ronnie Paskin <ronnie@megaputz.com>
2018-03-22 11:12:19 -0300

    Initial Commit

---
+++ xschat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xschat
+//
+//  Created by Ronnie Paskin on 18/03/22.
+//  Copyright © 2018 Megaputz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
