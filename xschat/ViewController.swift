5b372f5da02665f33942a95f38c7bdf0332cab34
Ronnie Paskin <ronnie@megaputz.com>
2018-03-22 11:46:59 -0300

    app simples com table view formando um "chat" mocked

--- xschat/ViewController.swift
+++ xschat/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputText: UITextField!
+    
+    var chatMessages = [("rpaskin","oi"), ("jsilva","tudo bom?"), ("rpaskin", "tudo"), ("ssalgado", "alguém quer um doce?")]
+    
+    @IBAction func enviarTouched(_ sender: UIButton) {
+        
+        if inputText.hasText {
+            let name = "rpaskin"
+            let message = inputText.text!
+
+            chatMessages.append((name, message))
+            chatTableView.reloadData()
+        }
+        else {
+            print("mensagem vazia!")
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatMessages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = chatTableView.dequeueReusableCell(withIdentifier: "cell")
+
+        let (name, message) = chatMessages[indexPath.row]
+        
+        cell?.textLabel?.text = name
+        cell?.detailTextLabel?.text = message
+
+        return cell!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
