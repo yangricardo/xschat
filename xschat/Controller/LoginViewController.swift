bd96410029fca30d48ec40cf88760b727febb032
Ronnie Paskin <ronnie@megaputz.com>
2018-04-16 16:50:34 -0300

    funcionando

---
+++ xschat/Controller/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  
+//
+//  Created by Ronnie Paskin on 18/04/16.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var chat_id: UITextField!
+    @IBOutlet weak var first_name: UITextField!
+    @IBOutlet weak var last_name: UITextField!
+    @IBOutlet weak var user_id: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
